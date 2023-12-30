904bc2cc704b70d6fe1e59adcdc74f4dcc045e93
wonjaegang <52266110+wonjaegang@users.noreply.github.com>
2023-12-30 17:30:44 +0900

    add creadBoard method(in progress) to inGameManager

--- scripts/inGameManager.gd
+++ scripts/inGameManager.gd
@@ -1,21 +1,30 @@
 extends Node2D
-
+var dict1 = {'line0': {0.5: {'type': 'Line',
+                             'subtype': 'normal',
+                             'toLine' : 1,
+                             'toY': 0.5},
+                       0.8: {'type': 'Line',
+                             'subtype': 'normal',
+                             'toLine' : 1,
+                             'toY': 0.5}},
+             'line1': {0.8: {'type': 'Line',
+                             'subtype': 'normal',
+                             'toLine' : 1,
+                             'toY': 0.5}}}
+                                
 func _ready():
-    var dict1 = {'line0': {0.5: {'type': 'Line',
-                                 'subtype': 'normal',
-                                 'toLine' : 1,
-                                 'toY': 0.5},
-                           0.8: {'type': 'Line',
-                                 'subtype': 'normal',
-                                 'toLine' : 1,
-                                 'toY': 0.5}},
-                 'line1': {0.8: {'type': 'Line',
-                                 'subtype': 'normal',
-                                 'toLine' : 1,
-                                 'toY': 0.5}}}
- 
-
-
+    createBoard(dict1)
+    
+func createBoard(boardInfoDict):
+    """
+    보드정보에 맞추어 보드 생성
+    """
+    var verticalNum = boardInfoDict.size()
+    for verticalLine in boardInfoDict.values():
+        for yRatio in verticalLine:
+            
+            print(yRatio)
+            print(verticalLine[yRatio])
 
 
 
