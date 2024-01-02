1e34de06566db2f9042e0f4ea8c9c9054a333768
wonjaegang <52266110+wonjaegang@users.noreply.github.com>
2024-01-02 23:01:05 +0900

    change boardInfo structure

--- scripts/inGameManager.gd
+++ scripts/inGameManager.gd
@@ -1,38 +1,29 @@
 extends Node2D
 
+const boardOffsetX = 300
+const boardOffsetY = 300
+const boardHeight = 700
+const boardHeight1 = 300
+
 signal generateVerticalLine(start, end)
 signal generateHorizontalLine(start, end, type)
 
-var dict1 = {'line0': {0.5: {'type': 'Line',
-                             'subtype': 'normal',
-                             'toLine' : 1,
-                             'toY': 0.5},
-                       0.8: {'type': 'Line',
-                             'subtype': 'normal',
-                             'toLine' : 1,
-                             'toY': 0.5}},
-             'line1': {0.8: {'type': 'Line',
-                             'subtype': 'normal',
-                             'toLine' : 1,
-                             'toY': 0.5}}}
-                                
+var boardMap1 = [[1, 0],
+                 [0, 1],
+                 [2, 2],]
+var lines1 = {type = ["nrm", "arR"],
+              colorDot = [Color(1, 0, 0), null],}                    
+                          
+                              
 func _ready():
-    createBoard(dict1)
+    createBoard(boardMap1, lines1)
     
-func createBoard(boardInfoDict):
+    
+func createBoard(boardMap, lines):
     """
     보드정보에 맞추어 보드 생성
     """
-    var verticalNum = boardInfoDict.size()
-    const df = 1
-    print(get_viewport_rect().size)
-    for verticalLine in boardInfoDict.values():
-        emit_signal("generateVerticalLine", Vector2(0, 0), Vector2(0, 0))
-        for yRatio in verticalLine:
-            
-            emit_signal("generateHorizontalLine", Vector2(0, 0), Vector2(0, 0), 'normal')
-            print(yRatio)
-            print(verticalLine[yRatio])
+    pass
 
 
 
