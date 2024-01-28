a94f90cc045f8bd80dabfee9dc96f02bb2af2fd6
wonjaegang <robinsam@naver.com>
2024-01-29 07:36:12 +0900

    simplify code

--- chapterScroll.gd
+++ chapterScroll.gd
@@ -1,7 +1,11 @@
 extends Control
+
 var isPressing = false
 var touchJustFinished = false
 var isInOrder = true
+
+var scrollMax
+var chapterNum
 
 var startY
 var currentY
@@ -13,10 +17,14 @@ var selectedChapterIdx = 1
 
 const DECEL = 1
 const STOP_VEL = 8  # STOP_VEL should be bigger than DECEL
+const CHAPTER_Y = 400  # Shold be even number
 
 
 func _ready():
-    $ScrollContainer.set_deferred("scroll_vertical", 200)
+    @warning_ignore("integer_division")
+    $ScrollContainer.set_deferred("scroll_vertical", CHAPTER_Y / 2)
+    scrollMax = $ScrollContainer/VBoxContainer.size.y - $ScrollContainer.size.y
+    chapterNum = $ScrollContainer/VBoxContainer.get_child_count() - 2
 
 
 func _process(_delta):
@@ -37,24 +45,23 @@ func _process(_delta):
             scrollVel -= sign(scrollVel) * DECEL
             $ScrollContainer.scroll_vertical -= scrollVel
             if $ScrollContainer.scroll_vertical == 0 or\
-               $ScrollContainer.scroll_vertical == $ScrollContainer/VBoxContainer.size.y - $ScrollContainer.size.y:
+               $ScrollContainer.scroll_vertical == scrollMax:
                 scrollVel = 0
             
         # 관성처리 종료 시 제 위치로 정렬
         else:
             if not isInOrder:
-                var gap = ($ScrollContainer.scroll_vertical + 200) % 400
+                @warning_ignore("integer_division")
+                var gap = ($ScrollContainer.scroll_vertical + CHAPTER_Y / 2) % CHAPTER_Y
                 if gap > 200 or $ScrollContainer.scroll_vertical == 0:
-                    $ScrollContainer.scroll_vertical += min(STOP_VEL, 400 - gap)
+                    $ScrollContainer.scroll_vertical += min(STOP_VEL, CHAPTER_Y - gap)
                 elif gap > 0:
                     $ScrollContainer.scroll_vertical -= min(STOP_VEL, gap)
                 else:
                     isInOrder = true
                     
     # 선택된 챕터 계산
-    selectedChapterIdx = $ScrollContainer.scroll_vertical / 400 + 1
-    if selectedChapterIdx > $ScrollContainer/VBoxContainer.get_child_count() - 2:
-        selectedChapterIdx = $ScrollContainer/VBoxContainer.get_child_count() - 2
+    selectedChapterIdx = min($ScrollContainer.scroll_vertical / CHAPTER_Y + 1, chapterNum)
     
     # 선택된 챕터 변경 시 로직
     if not lastSelectedChapterIdx == selectedChapterIdx:
